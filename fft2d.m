1d3d1f2e81ed388777fa7a8d6b3f9e1dec0e5b9a
Zhou Yichen <zhouyichen123@gmail.com>
2015-10-19 10:53:56 +0800

    More efficient way of reordering the rows and columns

--- fft2d.m
+++ fft2d.m
@@ -1,11 +1,7 @@
 function m = fft2d(m)
     [height, width] = size(m);
-    cols = num2cell(m, 1);
-    cols = bitrevorder(cols);
-    m = cell2mat(cols);
-    rows = num2cell(m, 2);
-    rows = bitrevorder(rows);
-    m = cell2mat(rows);
+    m = m(:,bitrevorder(1:width));
+    m = bitrevorder(m);
     m = fft1d(m, height, 1);
     m = fft1d(m, width, 0);
 end
--- ifft2d.m
+++ ifft2d.m
@@ -1,11 +1,7 @@
 function m = ifft2d(m)
     [height, width] = size(m);
-    cols = num2cell(m, 1);
-    cols = bitrevorder(cols);
-    m = cell2mat(cols);
-    rows = num2cell(m, 2);
-    rows = bitrevorder(rows);
-    m = cell2mat(rows);
+    m = m(:,bitrevorder(1:width));
+    m = bitrevorder(m);
     m = ifft1d(m, height, 1);
     m = ifft1d(m, width, 0);
 end
