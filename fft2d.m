c30e9ca115d91da59532551aa2f964ef05025a4f
Zhou Yichen <zhouyichen123@gmail.com>
2015-10-22 10:22:30 +0800

    Combine row and column fft1d into fft2d

--- fft2d.m
+++ fft2d.m
@@ -2,6 +2,26 @@ function m = fft2d(m)
     [height, width] = size(m);
     m = m(:,bitrevorder(1:width));
     m = bitrevorder(m);
-    m = fft1d(m, height, 1);
-    m = fft1d(m, width, 0);
+    N = max(height, width);
+    power = 1;
+    while N > power
+        step = power * 2;
+        e_theta = exp(- (0:power-1) * 1i *(2 * pi / step));
+        for start = 1:power
+            for a = start:step:N
+                b = a + power;
+                if power < height && b <= height
+                    temp = m(b, :) * e_theta(start);
+                    m(b, :) = m(a, :) - temp;
+                    m(a, :) = m(a, :) + temp;
+                end
+                if power < width && b <= width
+                    temp = m(:, b) * e_theta(start);
+                    m(:, b) = m(:, a) - temp;
+                    m(:, a) = m(:, a) + temp;
+                end;
+            end
+        end
+        power = step;
+    end
 end
