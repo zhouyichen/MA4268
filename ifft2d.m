a0e6b9d55eba97f04e0be6c7071b3140feebfdde
Zhou Yichen <zhouyichen123@gmail.com>
2015-10-22 11:09:30 +0800

    optimize for the case when height and width are equal

--- ifft2d.m
+++ ifft2d.m
@@ -2,27 +2,27 @@ function m = ifft2d(m)
     [height, width] = size(m);
     m = m(:,bitrevorder(1:width));
     m = bitrevorder(m);
-    N = max(height, width);
-    power = 1;
-    while N > power
-        step = power * 2;
-        e_theta = exp((0:power-1) * 1i *(2 * pi / step));
-        for start = 1:power
-            for a = start:step:N
-                b = a + power;
-                if power < height && b <= height
+    if height == width
+        power = 1;
+        while height > power
+            step = power * 2;
+            e_theta = exp((0:power-1) * 1i *(2 * pi / step));
+            for start = 1:power
+                for a = start:step:height
+                    b = a + power;
                     temp = m(b, :) * e_theta(start);
                     m(b, :) = m(a, :) - temp;
                     m(a, :) = m(a, :) + temp;
-                end
-                if power < width && b <= width
                     temp = m(:, b) * e_theta(start);
                     m(:, b) = m(:, a) - temp;
                     m(:, a) = m(:, a) + temp;
-                end;
+                end
             end
+            power = step;
         end
-        power = step;
+        m = m/(height * width);
+    else
+        m = ifft1d(m, height, 1);
+        m = ifft1d(m, width, 0);
     end
-    m = m/(width * height);
 end
